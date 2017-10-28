2a928732028c6d79765598ddae147361be39416b
Debashish Nayak <debashishzero@gmail.com>
2017-10-28 08:53:55 +0100

    Added stylesheets

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+
+import './styles/styles.scss';
+
 //import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />,

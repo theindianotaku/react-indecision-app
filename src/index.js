4450b4078f7485ce3e95d1a79c25bff539ce2e4c
Debashish Nayak <debashishzero@gmail.com>
2017-10-28 10:42:07 +0100

    Edited scripts to handle scss files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-import './styles/styles.scss';
+import './styles/styles.css';
 
 //import registerServiceWorker from './registerServiceWorker';
 
